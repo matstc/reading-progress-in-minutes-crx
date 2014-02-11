aa21501b392dab70d5ece18d1cd9d241ba81d667
Matthieu Tanguay-Carel <matthieutc@gmail.com>
2014-02-11 14:25:58 +0100

    isolates script into IIFE

--- contentscript.js
+++ contentscript.js
@@ -1,43 +1,43 @@
-console.log('reading progress enabled');
+console.log('Reading progress enabled');
 
-console.log('reading progress fired');
-display = document.createElement('div');
-display.style.backgroundColor = 'white';
-display.style.fontFamily = 'Ubuntu, Verdana, Sans-serif';
-display.style.opacity = '0.9';
-display.style.zIndex = 999;
-display.style.border = '1px solid #ccc';
-display.style.width = '140px';
-display.style.padding = '8px';
-display.style.position = 'fixed';
-display.style.borderRadius = '4px';
-display.style.fontSize = '16px';
-display.style.top = '10px'; 
+(function(){
+  var display = document.createElement('div');
+  display.style.backgroundColor = 'white';
+  display.style.fontFamily = 'Ubuntu, Verdana, Sans-serif';
+  display.style.opacity = '0.9';
+  display.style.zIndex = 999;
+  display.style.border = '1px solid #ccc';
+  display.style.width = '140px';
+  display.style.padding = '8px';
+  display.style.position = 'fixed';
+  display.style.borderRadius = '4px';
+  display.style.fontSize = '16px';
+  display.style.top = '10px'; 
 
-metric = document.createElement('span');
+  var metric = document.createElement('span');
 
-closeButton = document.createElement('span');
-closeButton.innerHTML = "&#10006;";
-closeButton.style.cursor = 'pointer';
-closeButton.style.float = 'right';
-closeButton.style.color = '#666';
-closeButton.addEventListener('click', function(){display.style.display = 'none';});
+  var closeButton = document.createElement('span');
+  closeButton.innerHTML = "&#10006;";
+  closeButton.style.cursor = 'pointer';
+  closeButton.style.float = 'right';
+  closeButton.style.color = '#666';
+  closeButton.addEventListener('click', function(){display.style.display = 'none';});
 
-display.appendChild(metric);
-display.appendChild(closeButton);
+  display.appendChild(metric);
+  display.appendChild(closeButton);
 
-document.body.appendChild(display);
+  document.body.appendChild(display);
 
-var redraw = function(){
-  display.style.left = window.innerWidth - 180 + 'px';
-  var wordCount = document.body.innerText.split(" ").length;
-  var wordsLeft = (1 - window.pageYOffset / document.body.scrollHeight) * wordCount;
-  var minutesLeft = wordsLeft / 250;
-  metric.innerHTML = Math.round(minutesLeft) + ' minutes left';
-};
+  var redraw = function(){
+    display.style.left = window.innerWidth - 180 + 'px';
+    var wordCount = document.body.innerText.split(" ").length;
+    var wordsLeft = (1 - window.pageYOffset / document.body.scrollHeight) * wordCount;
+    var minutesLeft = wordsLeft / 250;
+    metric.innerHTML = Math.round(minutesLeft) + ' minutes left';
+  };
 
-window.addEventListener('scroll', redraw);
-document.addEventListener('scroll', redraw);
-window.addEventListener('resize', redraw);
-redraw();
-
+  window.addEventListener('scroll', redraw);
+  document.addEventListener('scroll', redraw);
+  window.addEventListener('resize', redraw);
+  redraw();
+})();
